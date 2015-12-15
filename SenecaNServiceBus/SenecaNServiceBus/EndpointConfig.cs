1d220f1a21d9787a83b8206978c610f8c7cf7523
David Gonzalez <davidgonzalez@currencyfair.com>
2015-12-15 00:21:51 +0000

    Removed meaningless comment

--- SenecaNServiceBus/SenecaNServiceBus/EndpointConfig.cs
+++ SenecaNServiceBus/SenecaNServiceBus/EndpointConfig.cs
@@ -4,10 +4,6 @@ namespace SenecaNServiceBus
     using NServiceBus;
     using System;
 
-    /*
-		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
-		can be found here: http://particular.net/articles/the-nservicebus-host
-	*/
     public class EndpointConfig : IConfigureThisEndpoint
     {
         public static IBus Bus { get; private set; }
