d74befd458d4647a5bf6275aeedc03a7c647cf22
David Gonzalez <davidgonzalez@currencyfair.com>
2015-12-15 00:20:09 +0000

    Removed windows binaries

--- test/simple-test.js
+++ test/simple-test.js
@@ -1,11 +1,11 @@
 var seneca = require("seneca");
-
-seneca().use("seneca-nservicebus-transport")
+var senecaNServiceBus = require("../seneca-nservicebus-transport");
+seneca().use(senecaNServiceBus)
 .add({bar: "test"}, function(a, b) {
     b(null, {ping: "pong!"});
 }).listen({type: 'nservicebus'});
 
-var client = seneca().use("seneca-nservicebus-transport").client({type: "nservicebus"});
+var client = seneca().use(senecaNServiceBus).client({type: "nservicebus"});
 client.act({bar: "test"}, function(err, result){
     console.log(result);
 });
